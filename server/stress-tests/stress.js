7b1ac249c7c56df49653509505586e0034db8ef0
Eduardo Catalán <mcatalan@cartacorp.net>
2023-04-28 13:40:55 -0600

    update sterss

--- server/stress-tests/stress.js
+++ server/stress-tests/stress.js
@@ -1,27 +1,27 @@
 const loadtest = require('loadtest');
-const options = {
+
+const options1 = {
     url:'http://localhost:8080/movie/',
-    maxRequest: 100,
+    maxRequests: 500,
     concurrency: 10,
-    timeout: 5000,
-    method: 'GET'
+    timeout: 5000
 }
-/*
+
 const options2 = {
     url: 'http://localhost:8080/nombre',
-    maxRequest: 1000,
+    maxRequests: 1000,
     concurrency: 10,
     timeout: 5000
 }
 
 const options3 = {
     url: 'http://localhost:8080/carnet',
-    maxRequest: 10000,
+    maxRequests: 10000,
     concurrency: 20,
     timeout: 5000
-}*/
+}
 
-loadtest.loadTest(options, function(error, result) {
+loadtest.loadTest(options3, (error, result) => {
     if (error) {
         console.error('Test failed:', error);
     } else {
